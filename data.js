779d9bbdb1deed8d58f7ef126e1a56490782874d
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-07-15 14:47:32 +0100

    Refactored code to implement new features

---
+++ data.js
@@ -0,0 +1,35 @@
+export const posts = [
+  {
+      name: "Vincent van Gogh",
+      username: "vincey1853",
+      location: "Zundert, Netherlands",
+      avatar: "images/avatar-vangogh.jpg",
+      avataralt: "Avatar of user van gogh",
+      post: "images/post-vangogh.jpg",
+      postalt: "Self portrait of van gogh",
+      comment: "just took a few mushrooms lol",
+      likes: 21
+  },
+  {
+      name: "Gustave Courbet",
+      username: "gus1819",
+      location: "Ornans, France",
+      avatar: "images/avatar-courbet.jpg",
+      avataralt: "",
+      post: "images/post-courbet.jpg",
+      postalt: "",
+      comment: "i'm feelin a bit stressed tbh",
+      likes: 1
+  },
+  {
+      name: "Joseph Ducreux",
+      username: "jd1735",
+      location: "Paris, France",
+      avatar: "images/avatar-ducreux.jpg",
+      avataralt: "",
+      post: "images/post-ducreux.jpg",
+      postalt: "",
+      comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+      likes: 152
+  }
+]
--- index.js
+++ index.js
@@ -1,81 +1,68 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        avataralt: "Avatar of user van gogh",
-        post: "images/post-vangogh.jpg",
-        postalt: "Self portrait of van gogh",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        avataralt: "",
-        post: "images/post-courbet.jpg",
-        postalt: "",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 1
-    },
-    {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        avataralt: "",
-        post: "images/post-ducreux.jpg",
-        postalt: "",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from "./data.js";
+
 const postsEl = document.getElementById("posts");
 
-let likesBtn;
-let likesEl;
+let likesIcon = [];
+let likesImg = [];
 let postsHtml = "";
 
-for(let i = 0; i < posts.length; i++) {
+posts.forEach(function(post) {
+
     postsHtml += `<section>
                       <div class="post-user">
-                          <img class="avatar post-user-avatar" src="${posts[i].avatar}" alt="${posts[i].avataralt}">
+                          <img class="avatar post-user-avatar" src="${post.avatar}" alt="${post.avataralt}">
                           <div class="post-user-info">
-                            <p class="post-user-name">${posts[i].name}</p>
-                            <p>${posts[i].location}</p>
+                            <p class="bold-text">${post.name}</p>
+                            <p>${post.location}</p>
                           </div>
                       </div> 
                       <div class="post">
-                          <img class="post-img" src="${posts[i].post}" alt="${posts[i].postalt}">
+                          <img class="post-img" id="post-img-${post.username}" src="${post.post}" alt="${post.postalt}">
                           <div class="app-icons">
-                              <img id="like-btn-${posts[i].username}" class="app-icon" src="images/icon-heart.png" alt="Heart icon">
-                              <img class="app-icon" src="images/icon-comment.png" alt="Comment icon">
-                              <img class="app-icon" src="images/icon-dm.png" alt="DM icon">
+                              <div class="app-icon-bg" id="like-icon-${post.username}">
+                                <img class="app-icon" src="images/icon-heart.png" alt="Heart icon">
+                              </div>
+                              <div class="app-icon-bg">
+                                  <img class="app-icon" src="images/icon-comment.png" alt="Comment icon">
+                              </div>
+                              <div class="app-icon-bg">
+                                  <img class="app-icon" src="images/icon-dm.png" alt="DM icon">
+                              </div>
                           </div>
-                          <p class="post-likes bold-text" id="likes-${posts[i].username}">${posts[i].likes} like${posts[i].likes > 1 ? 's' : ''}</p>
-                          <p class="post-text"><span class="bold-text">${posts[i].username}</span> ${posts[i].comment}</p>
+                          <p class="post-likes bold-text" id="likes-${post.username}">${post.likes} like${post.likes > 1 ? 's' : ''}</p>
+                          <p class="post-text"><span class="bold-text">${post.username}</span> ${post.comment}</p>
                       </div>
-                      ${i + 1 != posts.length ? '<div class="spacer"></div>' : ''}
                   </section>`;
-}
+
+})
 
 postsEl.innerHTML = postsHtml;
 
-for(let i = 0; i < posts.length; i++) {
+/* Once the posts have been rendered we need to add the event listeners to each */
+posts.forEach(function(post, index) {
 
-  likesBtn = document.getElementById(`like-btn-${posts[i].username}`);
+    likesIcon[index] = document.getElementById(`like-icon-${post.username}`);
+    likesImg[index] = document.getElementById(`post-img-${post.username}`);
 
-  likesBtn.addEventListener("click", function() {
-    
-    posts[i].likes++;
+    likesIcon[index].addEventListener("click", function() {
+        incrementLikes(post);
+    });
 
-    likesEl = document.getElementById(`likes-${posts[i].username}`);
+    likesImg[index].addEventListener("click", function() {
+        incrementLikes(post);
 
-    likesEl.textContent = `${posts[i].likes} like${posts[i].likes > 1 ? 's' : ''}`;
+        /* Let the user know that clicking the image has clicked the like */
+        likesIcon[index].classList.add("liked");
 
-  });
+        setTimeout(function() {
+            likesIcon[index].classList.remove("liked")
+        },500);
+    });
+
+});
+
+function incrementLikes(post) {
+    post.likes++;
+
+    document.getElementById(`likes-${post.username}`).textContent = `${post.likes} like${post.likes > 1 ? 's' : ''}`;
 }
-
